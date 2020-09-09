6f5229917fd7e4df21edfa4bab6879a36c9d1aeb
Na1stibe <fheoa700000@naver.com>
2020-09-09 17:44:06 +0900

    cashmission page 뼈대 구축

--- src/cashmission.js
+++ src/cashmission.js
@@ -1,11 +1,32 @@
-import React , { Component } from 'react';
+import React, { Component } from "react";
 
 class Cashmission extends Component {
-    render(){
-        return <div>
-            <p>PERFORM IT <br/> FORM ANYWHERE <br/>AT ANYTIME</p>
+  render() {
+    return (
+      <div className="cashmission_cover_page">
+        <div className="cashmission_main_view">
+          <div className="cashmission_top_box">
+            <div className="top_box_little_text">
+              <div className="top_box_main_text">
+                <div className="top_box_sub_text"></div>
+              </div>
             </div>
-    }
+            <div className="top_box_main_img">
+              <div className="top_box_sub_img"></div>
+            </div>
+          </div>
+          <div className="cashmission_bottom_box">
+            <div className="bottom_box_main_img">
+              <div className="bottom_box_sub_img"></div>
+            </div>
+            <div className="bottom_box_main_text">
+              <div className="bottom_box_sub_text"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Cashmission;
