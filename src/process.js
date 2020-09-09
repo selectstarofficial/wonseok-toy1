3a950d1f9343c8b3938b94183e63f468ad893fe6
Na1stibe <fheoa700000@naver.com>
2020-09-09 15:52:14 +0900

    process page finish

--- src/process.js
+++ src/process.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import processimg from "./processimg.png";
+import "./process.css";
 
 class Process extends Component {
   render() {
@@ -11,11 +12,28 @@ class Process extends Component {
             <div className="process_main_text">
               AI DATA CROWEDSOURCING
               <div className="process_mid_text1">
-                <div className="process_sub_text1"></div>
+                Advance <span>Crowedsourcing Technology </span>based
+                <br />
+                on mathematical algorithms
+                <div className="process_sub_text1">
+                  To create scalable & robust crowdsourcing platform, we <br />
+                  developed the unique data quality control system using the
+                  <br />
+                  cutting-edge mathematical algorithms.
+                </div>
               </div>
-              <div className="process_mid_text2">
-                <div className="prcess_sub_text"></div>
-              </div>
+              {
+                <div className="process_mid_text2">
+                  AI assisted <span>Data Collection/Annotation</span>
+                  <div className="process_sub_text2">
+                    Our AI assisted data collection & annotation is efficient
+                    and
+                    <br /> effective. We provide easier annotation environment
+                    and we
+                    <br /> prevent the submission of useless ‘similar data’.
+                  </div>
+                </div>
+              }
             </div>
           </div>
         </div>
