068f6a3047bfe7f4a80268e8bac1fe91d79cde43
Na1stibe <fheoa700000@naver.com>
2020-09-09 18:24:20 +0900

    cashmission page finish

--- src/cashmission.js
+++ src/cashmission.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import "./cashmission.css";
+import cashmission_img1 from "./cashmission1.png";
+import cashmission_img2 from "./cashmission2.png";
 
 class Cashmission extends Component {
   render() {
@@ -7,20 +10,47 @@ class Cashmission extends Component {
         <div className="cashmission_main_view">
           <div className="cashmission_top_box">
             <div className="top_box_little_text">
+              CASHMISSION
               <div className="top_box_main_text">
-                <div className="top_box_sub_text"></div>
+                PERFORM IT <br />
+                FROM <br />
+                ANYWHERE
+                <br />
+                AT ANYTIME
+                <div className="top_box_sub_text">
+                  Cashmission is a crowdsourcing platform in both mobile
+                  <br />
+                  and web applications. With versatile working <br />
+                  environments, we collect diverse data quickly and
+                  <br /> accurately.
+                </div>
               </div>
             </div>
             <div className="top_box_main_img">
-              <div className="top_box_sub_img"></div>
+              <img src={cashmission_img1}></img>
             </div>
           </div>
           <div className="cashmission_bottom_box">
             <div className="bottom_box_main_img">
-              <div className="bottom_box_sub_img"></div>
+              <img src={cashmission_img2}></img>
             </div>
             <div className="bottom_box_main_text">
-              <div className="bottom_box_sub_text"></div>
+              SIMPLE MISSION <br />
+              ATTRACTIVE <br />
+              REWARDS
+              <div className="bottom_box_sub_text">
+                As missions pass our inspection process, earned points can be
+                <br />
+                exchanged for real cash!
+                <br />
+                <br />
+                Collected data is used for developing the cutting-edge
+                artificial <br />
+                <br />
+                intelligence for global companies. Join our journey of making
+                the world easier through data
+                <br /> revolution!
+              </div>
             </div>
           </div>
         </div>
