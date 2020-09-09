a046fda4e31e53222795bc791ffd53b2dde1c9b3
Na1stibe <fheoa700000@naver.com>
2020-09-09 15:25:00 +0900

    process 뼈대 구축

--- src/process.js
+++ src/process.js
@@ -1,11 +1,30 @@
-import React , { Component } from 'react';
+import React, { Component } from "react";
+import processimg from "./processimg.png";
 
 class Process extends Component {
-    render(){
-        return <div>
-            <p>AI DATA CROWDSOURCING</p>
+  render() {
+    return (
+      <div className="process_cover_page">
+        <div className="process_text_box">
+          <div className="process_little_text">
+            HOW IT WORKS
+            <div className="process_main_text">
+              AI DATA CROWEDSOURCING
+              <div className="process_mid_text1">
+                <div className="process_sub_text1"></div>
+              </div>
+              <div className="process_mid_text2">
+                <div className="prcess_sub_text"></div>
+              </div>
             </div>
-    }
+          </div>
+        </div>
+        <div className="process_img">
+          <img src={processimg}></img>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Process;
