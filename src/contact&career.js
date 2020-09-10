1e87a673e5e93d75018bd4c2edacc35ba9df5471
Na1stibe <fheoa700000@naver.com>
2020-09-10 22:40:13 +0900

    contact&career 뼈대 구축

--- src/contact&career.js
+++ src/contact&career.js
@@ -1,11 +1,25 @@
-import React , { Component } from 'react';
+import React, { Component } from "react";
+import "./contact&career.css";
 
 class Contact extends Component {
-    render(){
-        return <div>
-            <p>READY TO GET <br/>STARTED</p>
+  render() {
+    return (
+      <div className="cc_cover_page">
+        <dic className="cc_main_view">
+          <div className="cc_left_box">
+            <div className="cc_left_main_text">
+              <dic className="cc_left_sub_button"></dic>
             </div>
-    }
+          </div>
+          <div className="cc_right_box">
+            <div className="cc_right_main_text">
+              <div className="cc_right_sub_button"></div>
+            </div>
+          </div>
+        </dic>
+      </div>
+    );
+  }
 }
 
 export default Contact;
