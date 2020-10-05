d2879d586cdf149dcde7277404c5d5c4cb2643d0
Na1stibe <fheoa700000@naver.com>
2020-10-05 14:41:41 +0900

    Solution page error

--- src/A_Solutions/solutions.js
+++ src/A_Solutions/solutions.js
@@ -1,67 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 import "./solution.css";
-import solution_aiimg from "./img/solution_ai.png";
-import solution_user from "./img/solution_user.png";
-import solution_quality from "./img/solution_quality.png";
-import solution_mobile from "./img/solution_mobile.png";
+import solution_aiimg from "./img/solution_image1.c55d8896.jpg";
+import solution_user from "./img/solution_image2.2303e6bb.jpg";
+import solution_quality from "./img/solution_image3.8ebe61bf.jpg";
+import solution_mobile from "./img/solution_image4.cc2d8dfb.jpg";
 
-class Solution extends Component {
-  render() {
-    return (
-      <div className="solutions_coverpage">
-        <div className="solutions_imges">
-          <div className="solutions_aiimg">
-            <img src={solution_aiimg} alt="img"></img>
-            <div className="solutions_ai_text">
-              AI assisted Data Collection/Annotation
-              <div className="solutions_ai_subtext">
-                Our designated AI team discovers the most effective and
-                cutting-edge AI algorithms to collect and
-                <br />
-                annotate data
-              </div>
-            </div>
-          </div>
-          <div className="solutions_userimg">
-            <img src={solution_user} alt="img"></img>
-            <div className="solutions_user_text">
-              User-Centered Task Guidelines
-              <div className="solutions_user_subtext">
-                Our special guidance team maintains the data quality with the
-                best user-centric guidelines for the
-                <br />
-                crowed-workers.
-              </div>
-            </div>
-          </div>
-          <div className="solutions_qualityimg">
-            <img src={solution_quality} alt="img"></img>
-            <div className="solutions_quality_text">
-              Quality-Assuring Crowdsourcing
-              <br />
-              Algorithms
-              <div className="solutions_quality_subtext">
-                Our designated AI team discovers the most effective and
-                cutting-edge AI algorithms to collect and <br />
-                annotate data
-              </div>
-            </div>
-          </div>
-          <div className="solutions_mobileimg">
-            <img src={solution_mobile} alt="img"></img>
-            <div className="solutions_mobile_text">
-              Mobile Crowdsourcing to the World
-              <div className="solutions_mobile_subtext">
-                Our ubiquitous mobile crowdsourcing platform guarantees fast,
-                diverse, and affordable data
-              </div>
-            </div>
-          </div>
-          <div className="solutuins_empty"></div>
+const Solution = () => {
+  return (
+    <div className="Solution_cover">
+      <div className="Solution_main_page">
+        <div className="Solution_container">
+          <img src={solution_aiimg} alt="AI" />
+          <p className="Title">AI assisted Data Collection/Annotation</p>
+          <p className="Sub">
+            Our designated AI team discovers the most effective and cutting-edge
+            AI algorithms <br />
+            to collect and label data
+          </p>
+        </div>
+        <div className="Solution_container">
+          <img src={solution_user} alt="User" />
+          <p className="Title">Usr-Centered Task Guidelines</p>
+          <p className="Sub">
+            Our special guidance team maintains the data quality with the best
+            user-centric <br />
+            guidelines for the crowd-workers.{" "}
+          </p>
+        </div>
+        <div className="Solution_container">
+          <img src={solution_quality} alt="Quality" />
+          <p className="Title">Quality-Assuring Crowdsourcing AIgorithms</p>
+          <p className="Sub">
+            Our designated corwdsourcing team develops mathematical models to
+            maintain 99% <br />
+            accuracy of crowdsourced data
+          </p>
+        </div>
+        <div className="Solution_container">
+          <img src={solution_mobile} alt="Mobile" />
+          <p className="Title">Mobile Crowdsourcing to the World</p>
+          <p className="Sub">
+            Our ubiquitous mobile crowdsourcing platform guarantees fast,
+            diverse, and <br />
+            affordable data
+          </p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Solution;
